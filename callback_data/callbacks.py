0a70d97de6ce0565a6372ab96c41b7bf78efb5a4
Kirill T <sknxq1@gmail.com>
2024-04-29 22:21:48 +0300

    add callbacks package

---
+++ callback_data/callbacks.py
@@ -0,0 +1,5 @@
+class Callbacks:
+    clear_button_clb: str = 'clear_button_clb'
+    delete_button_clb: str = 'delete_button_clb'
+    equals_button_clb: str = 'equals_button_clb'
+    point_button_clb: str = 'point_button_clb'
