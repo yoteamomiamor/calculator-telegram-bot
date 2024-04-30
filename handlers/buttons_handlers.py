5e8024223eeec793b6c7bc487462c8ef618c42fb
Kirill T <sknxq1@gmail.com>
2024-04-30 15:32:50 +0300

    big refactoring

--- handlers/buttons_handlers.py
+++ handlers/buttons_handlers.py
@@ -6,100 +6,78 @@ from aiogram.filters import CommandStart, Command
 from lexicon import LEXICON
 from keyboards import calculator_keyboard
 from callback_data import Callbacks
+from utils import add_digit, add_point, add_minus, add_sign, clear_entry, delete_last
 
 
 router = Router()
+router.callback_query.filter()
+
+_operation_buttons_except_minus = (
+    LEXICON.mul_button_text,
+    LEXICON.div_button_text,
+    LEXICON.pls_button_text,
+)
 
 
 @router.callback_query(F.data == Callbacks.clear_button_clb)
 async def process_clear_button(callback: CallbackQuery):
-    if callback.message.text == LEXICON.empty_string:
-        pass
-    else:
+    updated_entry = clear_entry(callback.message.text)
+
+    if updated_entry:
         await callback.message.edit_text(
-            text=LEXICON.empty_string,
+            text=updated_entry,
             reply_markup=calculator_keyboard,
         )
 
 @router.callback_query(F.data == Callbacks.delete_button_clb)
 async def process_delete_button(callback: CallbackQuery):
-    entry = callback.message.text
+    updated_entry = delete_last(callback.message.text)
 
-    if entry == LEXICON.empty_string:
-        pass
-    elif len(entry) <= 1:
+    if updated_entry:
         await callback.message.edit_text(
-            text=LEXICON.empty_string,
-            reply_markup=calculator_keyboard,
-            )
-    else:
-        await callback.message.edit_text(
-            text=entry[:-1],
+            text=updated_entry,
             reply_markup=calculator_keyboard,
             )
 
 
 @router.callback_query(F.data.in_(('0123456789')))
 async def process_digit_button(callback: CallbackQuery):
-    entry = callback.message.text
+    updated_entry = add_digit(callback.message.text, callback.data)
 
-    if entry == LEXICON.empty_string:
+    await callback.message.edit_text(
+        text=updated_entry,
+        reply_markup=calculator_keyboard,
+        )
+
+
+@router.callback_query(F.data == Callbacks.point_button_clb)
+async def process_point_button(callback: CallbackQuery):
+    updated_entry = add_point(callback.message.text)
+
+    if updated_entry:
         await callback.message.edit_text(
-            text=callback.data,
-            reply_markup=calculator_keyboard,
-            )
-    else:
-        await callback.message.edit_text(
-            text=entry + callback.data,
+            text=updated_entry,
             reply_markup=calculator_keyboard,
             )
 
 
 @router.callback_query(F.data == LEXICON.min_button_text)
 async def process_minus_button(callback: CallbackQuery):
-    entry = callback.message.text
+    updated_entry = add_minus(callback.message.text)
 
-    if entry == LEXICON.empty_string:
+    if updated_entry:
         await callback.message.edit_text(
-            text=callback.data,
-            reply_markup=calculator_keyboard,
-            )
-    else:
-        await callback.message.edit_text(
-            text=callback.message.text + callback.data,
+            text=updated_entry,
             reply_markup=calculator_keyboard,
             )
 
 
-@router.callback_query(F.data == LEXICON.pls_button_text)
-async def process_minus_button(callback: CallbackQuery):
-    entry = callback.message.text
+@router.callback_query(F.data.in_(_operation_buttons_except_minus))
+async def process_operation_buttons(callback: CallbackQuery):
+    updated_entry = add_sign(callback.message.text, callback.data)
 
-    if entry == LEXICON.empty_string:
-        pass
-    elif entry in LEXICON.min_button_text:
+    if updated_entry:
         await callback.message.edit_text(
-            text=LEXICON.empty_string,
-            reply_markup=calculator_keyboard,
-            )
-    else:
-        await callback.message.edit_text(
-            text=callback.message.text + callback.data,
-            reply_markup=calculator_keyboard,
-            )
-
-
-@router.callback_query(F.data.in_((LEXICON.mul_button_text, LEXICON.div_button_text)))
-async def process_minus_button(callback: CallbackQuery):
-    entry = callback.message.text
-
-    if entry == LEXICON.empty_string:
-        await callback.message.edit_text(
-            text=callback.data,
-            reply_markup=calculator_keyboard,
-            )
-    else:
-        await callback.message.edit_text(
-            text=callback.message.text + callback.data,
+            text=updated_entry,
             reply_markup=calculator_keyboard,
             )
