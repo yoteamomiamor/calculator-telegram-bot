059a51beb40b9e3493210786cc3209e49a458f79
Kirill T <sknxq1@gmail.com>
2024-04-30 13:43:21 +0300

    update lexicon (delete spaces)

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -9,10 +9,10 @@ class LEXICON:
 
     clear_button_text: str = 'C'
     delete_button_text: str = '«'
-    equals_button_text: str = ' = '
+    equals_button_text: str = '='
     point_button_text: str = '.'
 
-    div_button_text: str = ' / '
-    mul_button_text: str = ' * '
-    min_button_text: str = ' - '
-    pls_button_text: str = ' + '
+    div_button_text: str = '/'
+    mul_button_text: str = '*'
+    min_button_text: str = '-'
+    pls_button_text: str = '+'
